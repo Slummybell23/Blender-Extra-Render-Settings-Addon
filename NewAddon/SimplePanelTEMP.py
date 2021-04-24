c35a14e8ed9e039b08d54ce91d04738648edf116
Slummybell23 <76507206+Slummybell23@users.noreply.github.com>
2021-04-23 19:08:42 -0500

    Add files via upload

---
+++ NewAddon/SimplePanelTEMP.py
@@ -0,0 +1,42 @@
+import bpy
+
+class SelectPanal(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    #BL lable is the side bar lable.
+    bl_label = "Extra Render Settings!!!"
+    bl_idname = "OBJECT_PT_ERsettings"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "EXTRA RENDER SETTINGS"
+
+    def draw(self, context):
+        #Draws all the icons
+        row = self.layout.row()
+        row.prop(context.window_manager.interface_vars, 'angles', expand=True)
+        self.layout.operator("object.rotation", text="Save")
+    
+
+
+
+
+
+
+
+        # layout = self.layout
+
+
+        # row = layout.row()
+        # row.operator('render.nothing', text="Do nothing")
+
+
+        
+        # row = layout.row()
+        # row.operator('render.shutdown', text="Shutdown")        
+        
+        # row = layout.row()
+        # row.operator('render.sleep', text="Sleep")
+
+        # row = self.layout.row()
+        # row.prop(context.window_manager.interface_vars, 'angles', expand=True)
+        # row.prop(context.window_manager.interface_vars, 'direction', expand=True)
+        # 
---
+++ NewAddon/__init__.py
@@ -0,0 +1,68 @@
+bl_info = {
+    "name": "TestPlug",
+    "author": "Your Name Here",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > Add > Mesh > New Object",
+    "description": "Adds a new Mesh Object",
+    "warning": "",
+    "doc_url": "",
+    "category": "Add Mesh",
+}
+
+
+import bpy
+
+
+from . simpleoperatorTEMP import (Global, No_OT_pe, Shut_OT_down, Sleep_OT_mode, Rotation)
+from . SimplePanelTEMP    import SelectPanal
+from . Interfacevars import InterfaceVars
+from bpy.app.handlers import persistent
+classes = (
+    No_OT_pe,
+    SelectPanal,
+    Shut_OT_down,
+    Sleep_OT_mode,
+    InterfaceVars,
+    Rotation
+
+)
+
+@persistent
+def action(dummy):
+    v = Global.v
+    
+    print(v)
+    if v == "BlendShutdown":
+        bpy.ops.wm.quit_blender()
+    elif v == "Shutdown":
+        import os
+        os.system('shutdown /p /f')
+    elif v == "Sleep":
+        import os
+        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+
+
+
+
+
+
+def register():
+    from bpy.utils import register_class
+
+    for cls in classes:
+        register_class(cls)
+    bpy.types.WindowManager.interface_vars = bpy.props.PointerProperty(type=InterfaceVars)
+    bpy.app.handlers.render_complete.append(action)
+
+def unregister():
+    from bpy.utils import unregister_class
+    bpy.app.handlers.render_complete.remove(action)
+    del bpy.types.WindowManager.interface_vars
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+
+
+if __name__ == "__main__":
+    register()
